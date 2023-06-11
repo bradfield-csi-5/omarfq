d551d276f8d06562b2992a6668b5f3911db9317f
Omar Flores <oflores@invidgroup.com>
2023-06-11 15:39:21 -0400

    Added exercise 1-5

---
+++ prep-phase/c/the_c_programming_language/chapter1/exercise_1-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*print Celsius-Farenheit table
+ for fahr = 0, 20, ..., 300*/
+
+int main() {
+  int fahr;
+
+  for (fahr = 300; fahr >= 0; fahr -= 20) {
+    printf("%3d %6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  }
+}
