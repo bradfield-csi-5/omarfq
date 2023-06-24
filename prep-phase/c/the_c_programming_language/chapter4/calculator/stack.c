f5e47a13eaee11560794ca89f150a01a5b36a447
Omar Flores <omarenriquefq@gmail.com>
2023-06-24 10:30:06 -0400

    Cleaned up calculator program

--- prep-phase/c/the_c_programming_language/chapter4/calculator/stack.c
+++ prep-phase/c/the_c_programming_language/chapter4/calculator/stack.c
@@ -8,16 +8,10 @@ double val[MAXVAL]; /* value stack */
 
 /* push:  push f onto value stack */
 void push(double f) {
-  printf("Before push, sp is: %d\n", sp);
-  if (sp < MAXVAL) {
+  if (sp < MAXVAL)
     val[sp++] = f;
-    printf("%d\n", sp);
-    for (int i = 0; i < MAXVAL; i++) {
-      printf("%f, ", val[i]);
-    }
-  } else
+  else
     printf("error: stack full, can't push %g\n", f);
-  printf("After push, sp is: %d\n", sp);
 }
 /* pop:  pop and return top value from stack */
 double pop(void) {
