bb756009ec24e5c9ace980a00de63320dcda7ed2
Omar Flores <omarenriquefq@gmail.com>
2024-04-26 18:27:36 -0400

    Write REPL using readline library

--- 7_distributed_systems/kvstore/cmd/main.go
+++ 7_distributed_systems/kvstore/cmd/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"github.com/chzyer/readline"
+)
+
+func main() {
+	rl, err := readline.New("Wirte something here > ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			break
+		}
+		fmt.Println(line)
+	}
+}
