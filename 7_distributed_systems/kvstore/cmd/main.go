f752413a2b27f2f2ff4cacf643382082d42c3129
Omar Flores <omarenriquefq@gmail.com>
2024-04-26 21:11:14 -0400

    Add commands get and set for in-memory storage and retrieval

--- 7_distributed_systems/kvstore/cmd/main.go
+++ 7_distributed_systems/kvstore/cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/chzyer/readline"
 )
 
@@ -14,11 +16,43 @@ func main() {
 
 	defer rl.Close()
 
+	kvStore := map[string]string{}
+	instructions := map[string]bool{"set": true, "get": true}
+
 	for {
 		line, err := rl.Readline()
 		if err != nil {
 			break
 		}
-		fmt.Println(line)
+
+		cmd := strings.Split(line, " ")
+		if len(cmd) != 2 {
+			fmt.Println("Error: Invalid input.")
+			continue
+		}
+
+		instruction, value := cmd[0], cmd[1]
+		if _, ok := instructions[instruction]; !ok {
+			fmt.Println("Error: Invalid instruction. Please make sure to use either 'get' or 'set'.")
+			continue
+		}
+
+		switch instruction {
+		case "set":
+			valueSlice := strings.Split(value, "=")
+			if len(valueSlice) != 2 {
+				fmt.Println("Error: Invalid key-value pair")
+				continue
+			}
+			key, val := valueSlice[0], valueSlice[1]
+			kvStore[key] = val
+			fmt.Printf("Inserted key: %s and value: %s\n", key, val)
+		case "get":
+			if _, ok := kvStore[value]; !ok {
+				fmt.Println("Error: The key does not exist in the key-value store.")
+				continue
+			}
+			fmt.Println(kvStore[value])
+		}
 	}
 }
